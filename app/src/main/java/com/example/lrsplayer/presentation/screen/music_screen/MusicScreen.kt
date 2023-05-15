870efbcbe8f49c45cb180feb222e24820004451a
Олег <playegrovoy@gmail.com>
2023-05-15 17:14:05 +0300

    header

--- app/src/main/java/com/example/lrsplayer/presentation/screen/music_screen/MusicScreen.kt
+++ app/src/main/java/com/example/lrsplayer/presentation/screen/music_screen/MusicScreen.kt
@@ -5,21 +5,36 @@ import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.lrsplayer.R
+import com.example.lrsplayer.presentation.theme.sf_pro_text
 import com.example.lrsplayer.until.ThemeColors
 
 @Composable
@@ -39,20 +54,63 @@ fun MusicScreen(
     
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colors.main_background)
+            .padding(top = 24.dp)
     ) {
-        
-        items(state.data, {it.id!!}){
-            Text(text = it.name)
-        }
-
         item {
-            Button(onClick = {
-                launcher.launch("audio/*")
-            }) {
-                Text(text = "click")
-            }  
+            Box(
+                modifier = Modifier.fillMaxWidth().padding(start = 13.dp, end = 32.dp)
+            ){
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(
+                            onClick = {  },
+                            modifier = Modifier
+                                .width(16.dp)
+                                .height(13.dp)
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.menu),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .width(16.dp)
+                                    .height(13.dp),
+                                tint = colors.title
+                            )
+                        }
+                        Text(
+                            text = "SniPlayer",
+                            fontSize = 20.sp,
+                            fontFamily = sf_pro_text,
+                            fontWeight = FontWeight.ExtraBold,
+                            color = colors.title
+                        )
+                    }
+                    IconButton(
+                        onClick = {  },
+                        modifier = Modifier
+                            .size(30.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_search_24),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(30.dp),
+                            tint = colors.title
+                        )
+                    }
+
+                }
+            }
         }
     }
 
